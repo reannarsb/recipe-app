6e10a8c2fe7e8871cc08f3ca2901f6aa8c31fae0
reannarsb <115964061+reannarsb@users.noreply.github.com>
2024-08-04 15:28:07 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <main className="p-8">
             <header className="text-center mb-8">
-                <h1 className="text-4xl font-bold">Recipe Finder</h1>
+                <h1 className="text-4xl font-bold">Delightful</h1>
                 <h2 className="text-2xl">Discover delicious recipes</h2>
             </header>
             <Image />
